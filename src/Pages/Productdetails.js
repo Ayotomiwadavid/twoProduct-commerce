8a3e0bd2e63d24f68c40e0a3b63ef308c2093c31
Techthoth <ayotomiwadavid@proton.me>
2024-07-26 15:21:26 +0100

    saving progress

--- src/Pages/Productdetails.js
+++ src/Pages/Productdetails.js
@@ -8,11 +8,31 @@ import { ProductData } from "../components/ProductsData";
 const Productdetails = () => {
   const { id } = useParams();
   const [quantity, setQuantity] = useState(1);
+  const [imageIndex, setImageIndex] = useState(0);
 
   // Ensure the correct type comparison (string vs number)
   let detailedProduct = ProductData.find(
     (data) => data.productId.toString() === id
   );
+
+
+  const increaseQuantity = () => {
+    setQuantity(prevValue => prevValue + 1);
+  }
+
+  const decreaseQuantity = () => {
+    setQuantity(prevValue => {
+      if (prevValue !== 1) {
+       return prevValue - 1
+      } else {
+        return prevValue = 1
+      }
+    });
+  }
+
+  const changeImage = (index) => {
+    setImageIndex(index);
+  }
 
   if (!detailedProduct) {
     return (
@@ -46,11 +66,11 @@ const Productdetails = () => {
         </aside>
         <article className="flex flex-col justify-center items-center gap-4 px-3 w-full md:my-3 md:justify-between md:flex-row">
           <div className="flex flex-col w-full items-center justify-start md:justify-center gap-5 md:w-1/2 h-lvh">
-            <div className="w-full items-center justify-center">
+            <div className="w-full flex items-center justify-center">
               <img
-                src={detailedProduct.ProductImage[0]}
+                src={detailedProduct.ProductImage[imageIndex]}
                 alt=""
-                className="w-full"
+                className="w-[50%]"
               />
             </div>
             <div className="flex w-full gap-3 items-center justify-center">
@@ -61,6 +81,7 @@ const Productdetails = () => {
                   className="w-[90px] h-[90px]"
                   key={index}
                   id={index}
+                  onClick={() => changeImage(index)}
                 />
               ))}
             </div>
@@ -70,11 +91,12 @@ const Productdetails = () => {
               {detailedProduct.ProductTitle}
             </h2>
             <p>{detailedProduct.ProductDescription}</p>
+            <h1 className="tracking-wider text-xl py-3">{`$${detailedProduct.ProductPrice}.00`}</h1>
             <div className="flex gap-5 items-center justify-start w-full">
               <div className="flex items-center justify-between gap-2 bg-[#f5f5f5] px-4 h-[45px] w-[150px]">
-                <button className="cursor-pointer px-3 font-bold text-lg">+</button>
+                <button className="cursor-pointer px-3 font-bold text-lg" onClick={increaseQuantity}>+</button>
                 {quantity}
-                <button className="cursor-pointer px-3 font-bold text-lg">-</button>
+                <button className="cursor-pointer px-3 font-bold text-lg" onClick={decreaseQuantity}>-</button>
               </div>
               <button className="capitalize h-[45px] flex justify-center items-center text-xl rounded-lg my-4 py-3 font-bold bg-black text-white px-4">
                 Check out
